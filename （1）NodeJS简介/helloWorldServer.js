69804651b143016aded918c014229da869ca8534
Yzhaoyiming <1239069678@qq.com>
2019-09-02 17:00:36 +0800

    message

---
+++ （1）NodeJS简介/helloWorldServer.js
@@ -0,0 +1,9 @@
+const http=require("http");
+
+var server=http.createServer(function(req,res){
+    res.write("hello world");
+    //响应结束
+    res.end();
+});
+server.listen(8080);
+console.log("server is listening 8080");
