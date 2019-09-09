fa7423c6edc37ce739329cbd8e5aabe28c36f532
Yzhaoyiming <1239069678@qq.com>
2019-09-09 16:13:31 +0800

    message

---
+++ （2）NodeJS全局API/homework/nodeConsole.js
@@ -0,0 +1,9 @@
+var user={
+    name:"zhaoyiming",
+    age:19,
+    qq:"1239069678"
+}
+console.log("username is %s",user.name);
+console.log("age is %d",user.age);
+console.log("json is %j",user);
+
