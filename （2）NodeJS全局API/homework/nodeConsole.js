50c868d0b3f22516ad1c11bc0f05cb66cfd1e9c7
赵一鸣 <33643068+Yzhaoyiming@users.noreply.github.com>
2019-09-20 09:16:29 +0800

    Delete nodeConsole.js

--- （2）NodeJS全局API/homework/nodeConsole.js
+++
@@ -1,9 +0,0 @@
-var user={
-    name:"zhaoyiming",
-    age:19,
-    qq:"1239069678"
-}
-console.log("username is %s",user.name);
-console.log("age is %d",user.age);
-console.log("json is %j",user);
-
